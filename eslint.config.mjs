2077f3893eb7397e906b3d2243c7ab37c6f5ae3d
Etao <realecto@users.noreply.github.com>
2025-03-18 00:32:55 +1000

    Disabled some eslint rules for deployment

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -9,8 +9,21 @@ const compat = new FlatCompat({
   baseDirectory: __dirname,
 });
 
-const eslintConfig = [
+let eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
 ];
+
+if (process.env.NODE_ENV === "production") {
+  eslintConfig = [
+    ...eslintConfig,
+    {
+      rules: {
+        "@typescript-eslint/no-unused-vars": "off",
+        "react-hooks/exhaustive-deps": "off",
+        "@next/next/no-img-element": "off",
+      },
+    },
+  ];
+}
 
 export default eslintConfig;
