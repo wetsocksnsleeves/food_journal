6d371b227d072b7dec2faeb217cfbb2ecf0eee61
wetsocksnsleeves <ectos69@gmail.com>
2025-11-21 05:22:24 +1000

    fix: Turned of reasoning to improve performance

--- app/api/queryai/route.ts
+++ app/api/queryai/route.ts
@@ -53,7 +53,7 @@ export async function POST(request: Request) {
                         ],
                     },
                 ],
-                reasoning: { enabled: true },
+                reasoning: { enabled: false },
             }),
         },
     );
